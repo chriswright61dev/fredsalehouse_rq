56902aa8b7eb405b6a0c75bf1d55146d35d84902
Chris Wright <chriswrightdev@gmail.com>
2021-11-19 21:47:56 +0000

    header venue description getting data

--- src/modules/Header/HeaderVenueDescription/HeaderVenueDescription.js
+++ src/modules/Header/HeaderVenueDescription/HeaderVenueDescription.js
@@ -1,5 +1,17 @@
+import useVenue from "../../../data/reactQueryHooks/useVenue";
 function HeaderVenueDescription() {
-  return <div>'HeaderVenueDescription'</div>;
+  const venue = useVenue();
+
+  if (venue.status === "success") {
+    const venueData = venue.data[0];
+    return (
+      <div className="header-venue-description">
+        {venueData.venue_description}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default HeaderVenueDescription;
