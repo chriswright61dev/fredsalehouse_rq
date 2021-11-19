5be899cb22af0c6ad385c206c6b9636c561cd5dc
Chris Wright <chriswrightdev@gmail.com>
2021-11-19 21:25:35 +0000

    other venue modules fetching data

--- src/modules/venue/VenueAbout/VenueAbout.js
+++ src/modules/venue/VenueAbout/VenueAbout.js
@@ -1,5 +1,16 @@
+import useVenue from "../../../data/reactQueryHooks/useVenue";
 function VenueAbout() {
-  return <div>'venue about'</div>;
+  const venue = useVenue();
+  if (venue.status === "success") {
+    const venueData = venue.data[0];
+    return (
+      <div>
+        <p> {venueData.venue_about}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default VenueAbout;
--- src/modules/venue/VenueOpeningTimes/VenueOpeningTimes.js
+++ src/modules/venue/VenueOpeningTimes/VenueOpeningTimes.js
@@ -1,5 +1,25 @@
+import useVenue from "../../../data/reactQueryHooks/useVenue";
 function VenueOpeningTimes() {
-  return <div>'venue opening times'</div>;
+  const venue = useVenue();
+  if (venue.status === "success") {
+    const venueData = venue.data[0];
+    const special = venueData.venue_opening_special;
+    return (
+      <div>
+        <h3>Opening Times</h3>
+        <p>Monday: {venueData.venue_opening_monday}</p>
+        <p>Tuesday: {venueData.venue_opening_tuesday}</p>
+        <p>Wednesday: {venueData.venue_opening_wednesday}</p>
+        <p>Thursday: {venueData.venue_opening_thursday}</p>
+        <p>Friday: {venueData.venue_opening_friday}</p>
+        <p>Saturday: {venueData.venue_opening_saturday}</p>
+        <p>Sunday: {venueData.venue_opening_sunday}</p>
+        {special ? <p> vs.venue_opening_special</p> : null}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default VenueOpeningTimes;
