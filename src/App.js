3fba44de8698d2038d122fe15f100523a5b22eb0
Chris Wright <chriswrightdev@gmail.com>
2021-11-19 20:56:26 +0000

    fetched some data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import "./AppColours.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Home from "./pages/Home";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
+import useVenue from "../data/reactQueryHooks/useVenue";
+
 function Home() {
-  return <div>'home'</div>;
+  const venue = useVenue();
+  if (venue.status === "success") {
+    console.log(venue.data);
+    return <div>{venue.data[0].venue_about}</div>;
+  } else {
+    return null;
+  }
 }
 
 export default Home;
