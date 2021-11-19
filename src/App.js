62a17f9d3699e416fabb47d71a24ace66d5869c5
Chris Wright <chriswrightdev@gmail.com>
2021-11-19 20:43:15 +0000

    home page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import "./AppColours.css";
+
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
