bc7e4d448c7327aac3c36734c21eb925d8315fca
Chris Wright <chriswrightdev@gmail.com>
2021-11-19 21:18:47 +0000

    venue module fetches data

--- src/modules/venue/Venue/Venue.js
+++ src/modules/venue/Venue/Venue.js
@@ -1,5 +1,22 @@
+import useVenue from "../../../data/reactQueryHooks/useVenue";
+
 function Venue() {
-  return <div>'venue'</div>;
+  const venue = useVenue();
+  if (venue.status === "success") {
+    const venueData = venue.data[0];
+    return (
+      <div>
+        <h1>{venueData.venue_name}</h1>
+        <p> {venueData.venue_address_1}</p>
+        <p> {venueData.venue_address_2}</p>
+        <p> {venueData.venue_address_3}</p>
+        <p> {venueData.venue_postcode}</p>
+        <p> {venueData.venue_telephone}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Venue;
