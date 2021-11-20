b12d885c65a598c97f4b96e9fff66bb5faaabb72
Chris Wright <chriswrightdev@gmail.com>
2021-11-20 16:29:33 +0000

    date utility functions

--- src/components/EventCard/EventDate/EventDate.js
+++ src/components/EventCard/EventDate/EventDate.js
@@ -1,5 +1,17 @@
-function EventDate() {
-  return <div>event date</div>;
+import React from "react";
+import { DateFormat } from "../../../utilities/utilities.js";
+import "./EventDate.css";
+function EventDate(props) {
+  const theDateData = DateFormat(props.date);
+
+  return (
+    <h5 className="event_card_date">
+      {theDateData.dayofweek} {theDateData.daysuffix} {theDateData.month} at{" "}
+      {theDateData.hour}
+      {theDateData.mins}
+      {theDateData.ampm}
+    </h5>
+  );
 }
 
 export default EventDate;
---
+++ src/utilities/utilities.js
@@ -0,0 +1,92 @@
+export function DateFormat(dateTimeString) {
+  // fix data from the api which is a specific string
+  const datesarray = dateTimeString.split(" ");
+  // console.log("datesarray", datesarray);
+  const DateTimeObject = {};
+  DateTimeObject.dayofweek = datesarray[0].slice(0, -1);
+  DateTimeObject.day = datesarray[2].slice(0, -1);
+  DateTimeObject.month = datesarray[1];
+  DateTimeObject.year = datesarray[3];
+  DateTimeObject.hour24 = datesarray[5].slice(0, 2);
+  DateTimeObject.mins = datesarray[5].slice(3, 5);
+  // fix some formatting
+  DateTimeObject.hour = formatHour(DateTimeObject.hour24);
+  DateTimeObject.mins = formatMinutes(DateTimeObject.mins);
+  DateTimeObject.ampm = formatAmPm(DateTimeObject.hour24);
+  DateTimeObject.daysuffix = dateSuffix(DateTimeObject.day);
+  return DateTimeObject;
+}
+
+function formatHour(hourString) {
+  let hour = parseInt(hourString);
+  if (hour <= 12) {
+    return hour.toString();
+  } else return (hour - 12).toString();
+}
+
+function formatMinutes(minutesString) {
+  // don't want :00pm
+  return minutesString === "00" ? "" : ":" + minutesString;
+}
+
+function formatAmPm(hourString) {
+  let hour = parseInt(hourString);
+  if (hour < 12) {
+    return "am";
+  } else {
+    return "pm";
+  }
+}
+
+function dateSuffix(numberText) {
+  let numberAndSuffixText = numberText;
+  if (numberText === "1") {
+    numberAndSuffixText = "1st";
+  } else if (numberText === "2") {
+    numberAndSuffixText = "2nd";
+  } else if (numberText === "3") {
+    numberAndSuffixText = "3rd";
+  } else if (numberText === "21") {
+    numberAndSuffixText = "21st";
+  } else if (numberText === "22") {
+    numberAndSuffixText = "22nd";
+  } else if (numberText === "23") {
+    numberAndSuffixText = "23rd";
+  } else {
+    numberAndSuffixText += "th";
+  }
+
+  return numberAndSuffixText;
+}
+
+function dayOfWeekName(number) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[number];
+}
+
+function monthName(number) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return months[number];
+}
