38df639fc4ef9e28640c5549eec66f4db4e5abcf
Chris Wright <chriswrightdev@gmail.com>
2021-11-20 15:50:45 +0000

    dated event receiving data

---
+++ src/modules/events/DatedEvents/DatedEvents.js
@@ -0,0 +1,39 @@
+import useEvents from "../../../data/reactQueryHooks/useEvents";
+
+import EventCard from "../../../components/EventCard/EventCard";
+
+import "./DatedEvents.css";
+function DatedEvents() {
+  const events = useEvents();
+
+  if (events.status === "success") {
+    const eventsData = events.data;
+
+    if (eventsData.length === 0) {
+      return null;
+    } else {
+      return (
+        <div className="dated_events">
+          {eventsData.map((anEvent) => {
+            return (
+              <EventCard
+                link="events"
+                key={anEvent.id}
+                id={anEvent.id}
+                name={anEvent.event_name}
+                date={anEvent.event_date_long}
+                description={anEvent.event_description}
+                admission={anEvent.event_admission}
+                imageURL={anEvent.event_poster}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+  } else {
+    return null;
+  }
+}
+
+export default DatedEvents;
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,11 +1,13 @@
 import PosterEvents from "../modules/events/PosterEvents/PosterEvents";
 import RegularEvents from "../modules/events/RegularEvents/RegularEvents";
+import DatedEvents from "../modules/events/DatedEvents/DatedEvents";
 function Events() {
   return (
     <div>
       'events page'
       <PosterEvents />
       <RegularEvents />
+      <DatedEvents />
     </div>
   );
 }
