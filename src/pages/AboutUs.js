7a1889f76f8ce3e914ea6058d4d631e21a91fe79
Chris Wright <chriswrightdev@gmail.com>
2021-11-20 14:10:09 +0000

    about us adding data

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,21 @@
+import Venue from "../modules/venue/Venue/Venue";
+import VenueAbout from "../modules/venue/VenueAbout/VenueAbout";
+import VenueOpeningTimes from "../modules/venue/VenueOpeningTimes/VenueOpeningTimes";
+import BeerNews from "../modules/news/BeerNews";
+import OfferNews from "../modules/news/OfferNews";
+import GuestAles from "../modules/drinks/GuestAles/GuestAles";
+
 function AboutUs() {
-  return <div>'about us'</div>;
+  return (
+    <>
+      <Venue />
+      <VenueAbout />
+      <VenueOpeningTimes />
+      <BeerNews />
+      <OfferNews />
+      <GuestAles />
+    </>
+  );
 }
 
 export default AboutUs;
