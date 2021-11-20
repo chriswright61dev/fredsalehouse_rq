6cb965f23281d4e9b1e8e3f1b7837b8df0e71076
Chris Wright <chriswrightdev@gmail.com>
2021-11-20 14:14:07 +0000

    gmap getting data

--- src/components/GMap/GMap.js
+++ src/components/GMap/GMap.js
@@ -1,5 +1,24 @@
+import useVenue from "../../data/reactQueryHooks/useVenue";
+import "./Gmap.css";
+
 function GMap() {
-  return <div>'map'</div>;
+  const venue = useVenue();
+  const venueData = venue.data[0];
+  const baseUrl = "https://www.google.com/maps/embed/v1/";
+  const apikey = "AIzaSyAUplwoF60k0_6w55l-v7JDcG7dSNibq6I";
+  const Gkey = "?key=" + apikey;
+
+  const center = `&center=${venueData.venue_gmap_latitude},${venueData.venue_gmap_longitude}`;
+
+  const mapquery = `&q=${venueData.venue_map_query_text}`;
+  const zoom = "&zoom=20";
+  const mapsrc = baseUrl + "place" + Gkey + mapquery + center + zoom;
+
+  return (
+    <div className="gmap">
+      <iframe title="map" width="100%" height="100%" src={mapsrc}></iframe>
+    </div>
+  );
 }
 
 export default GMap;
